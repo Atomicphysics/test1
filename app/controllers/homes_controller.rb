6ca268dca1af8dda9996eb4189b7543e3b419f4e
nhw <amazinghw@naver.com>
2019-11-15 08:23:05 +0000

    테스트 업로드

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,22 @@
+class HomesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  
+  def show2
+    @apple = params[:apple]
+    @banana = params[:banana]
+  
+  end
+  
+end
+
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  
+  root 'homes#index'
+  
+  
+  get 'homes/index'
+  get 'homes/index2'
+  get 'homes/create'
+  get 'homes/show'
+  get 'homes/edit'
+  get 'homes/name'
+  get 'homes/show2'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
